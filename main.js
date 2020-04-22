80537b2d8a4bc5b6fbe98146046082255266ca64
John Bain <johnknoxbain@gmail.com>
2020-04-22 15:14:20 -0500

    initial commit

---
+++ main.js
@@ -0,0 +1,35 @@
+fetch("http://localhost:8088/food")
+    .then(response => response.json())
+    .then(myParsedFoods => {
+        myParsedFoods.forEach(food => {
+            console.log(food) // Should have a `barcode` property
+
+            // Now fetch the food from the Food API
+            fetch(`https://world.openfoodfacts.org/api/v0/product/${food.barcode}.json`)
+                .then(response => response.json())
+                .then(productInfo => {
+                    if (productInfo.product.ingredients_text) {
+                      food.ingredients = productInfo.product.ingredients_text
+                    } else {
+                      food.ingredients = "no ingredients listed"
+                    }
+
+                    // Produce HTML representation
+                    const foodAsHTML = foodFactory(food)
+
+                    // Add representaiton to DOM
+                    addFoodToDom(foodAsHTML)
+                })
+        })
+    })
+
+function foodFactory(object){
+    return `<div><h1>${object.name}</h1>
+    <p>${object.ethnicity}</p>
+    <p>${object.category}</p>
+    <p>${object.ingredients}</p></div>`
+}
+
+function addFoodToDom(food) {
+    document.querySelector("body").innerHTML += food
+}
